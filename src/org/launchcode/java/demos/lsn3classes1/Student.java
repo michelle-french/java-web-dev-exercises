202f943da38ffb60bbdde05789ce216b3d19e443
michelle-french <80438737+michelle-french@users.noreply.github.com>
2021-07-24 22:19:32 -0500

    exercises 4

--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -10,4 +10,38 @@ public class Student {
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+    public Student(String name, int studentId, int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String aName) {
+        this.name = aName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+    public void setStudentId(int aStudentId) {
+        this.studentId = aStudentId;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+    public void setNumberOfCredits(int aNumberOfCredits) {
+        this.numberOfCredits = aNumberOfCredits;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+    protected void setGpa(int aGpa) {
+        this.gpa = aGpa;
+    }
 }
