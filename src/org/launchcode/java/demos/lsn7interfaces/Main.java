14cb6814340af311a1b157cbd68601fe31749f50
michelle-french <80438737+michelle-french@users.noreply.github.com>
2021-08-25 12:16:27 -0500

    8 Exercises

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -1,12 +1,28 @@
 package org.launchcode.java.demos.lsn7interfaces;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args){
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        Comparator comparator = new FlavorComparator();
+
+        Comparator coneComp = new ConeComparator();
+
+        System.out.println(flavors);
+
+        flavors.sort(comparator);
+
+        System.out.println(flavors);
+
+        System.out.println(cones);
+
+        cones.sort(coneComp);
+
+        System.out.println(cones);
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
